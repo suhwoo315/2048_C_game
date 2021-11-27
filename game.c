3af2f01fb6863c534f831eb5c0851337773b93ff
Veronica Suhwoo Yoon <yeopil315@gmail.com>
2021-11-28 00:48:44 +0900

    working on move/merge mechanics
    * implemented move down function
    => focused on making it work
    => not so clean, needs to be trimmed

--- game.c
+++ game.c
@@ -9,7 +9,10 @@ void titleScreen();
 void resetGame();
 void spawnBlock();
 void drawBoard();
-void mergeBlock();
+void moveUp();
+void moveLeft();
+void moveDown();
+void moveRight();
 
 unsigned score;
 unsigned board[4][4];
@@ -29,19 +32,27 @@ int main(void){
         {
         case 'w':
         case 65:
-            printf("up\n"); // implement this
+            moveUp();
+            spawnBlock();
+            drawBoard();
             break;
         case 'a':
         case 68:
-            printf("left\n"); // implement this
+            moveLeft();
+            spawnBlock();
+            drawBoard();
             break;
         case 's':
         case 66:
-            printf("down\n"); // implement this
+            moveDown();
+            spawnBlock();
+            drawBoard();
             break;
         case 'd':
         case 67:
-            printf("right\n"); // implement this
+            moveRight();
+            spawnBlock();
+            drawBoard();
             break;
         case 'q':
             printf("quit\n"); // implement this
@@ -68,22 +79,14 @@ void setBufferedInput(int enable) {
 	static int enabled = 1;
 	static struct termios old;
 	struct termios new;
-
 	if (enable && !enabled) {
-		// restore the former settings
 		tcsetattr(STDIN_FILENO,TCSANOW,&old);
-		// set the new state
 		enabled = 1;
 	} else if (!enable && enabled) {
-		// get the terminal settings for standard input
 		tcgetattr(STDIN_FILENO,&new);
-		// we want to keep the old setting to restore them at the end
 		old = new;
-		// disable canonical mode (buffered i/o) and local echo
 		new.c_lflag &=(~ICANON & ~ECHO);
-		// set the new settings immediately
 		tcsetattr(STDIN_FILENO,TCSANOW,&new);
-		// set the new state
 		enabled = 0;
 	}
 }
@@ -110,6 +113,7 @@ void spawnBlock(){
 
 void drawBoard(){
     // temporary
+    printf("\nscore : %d\n", score);
     for(int i = 0; i < 4; i++){
         for(int j = 0; j < 4; j++){
             printf("%d ", board[i][j]);
@@ -118,5 +122,63 @@ void drawBoard(){
     }
 }
 
+void moveUp(){}
+
+void moveLeft(){}
+
+void moveDown(){
+    int num1 = 0, num2 = 0;
+    int index = 3;
+    for(int j = 0; j < 4; j++){
+        for(int i = 3; i >= 0; i--){
+            if (board[i][j] == 0) continue;
+            if (num1 == 0) {
+                num1 = board[i][j];
+            }
+            else if (num2 == 0) {
+                num2 = board[i][j];
+                if (num1 == num2){
+                    board[index][j] = num1 << 1;
+                    index--;
+                    score += num1 << 1;
+                    num1 = num2 = 0;
+                }
+                else {
+                    board[index][j] = num1;
+                    index--;
+                    num1 = num2;
+                    num2 = 0;
+                }
+            }
+        }
+        if (num1 != 0) {
+            board[index][j] = num1;
+            index--;
+        }
+        num1 = num2 = 0;
+        for(int i = 0; i <= index; i++) board[i][j] = 0;
+        index = 3;
+    }
+}
+
+void moveRight(){}
+
 /* IDEAS */
 // best score or score board => file I/O
+/*
+    variable a, b to temporarily save number
+    variable c for slot number
+
+    if it is 1, don't do anything
+
+    save first number to a, and second number to b
+    if a = b,
+        save bit-shifted value to slot c
+        c++
+        score += bit-shifted value
+    if a != b,
+        save a value to slot c
+        c++
+        move b to a
+        save next number to b
+*/
